1a4c5fff66693d605f164a335d0c99d11528232a
Math <malopesflo@gmail.com>
2025-11-18 22:20:09 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,135 @@
+#!/bin/sh
+
+echo "WARNING. THIS PROCESS WILL DELETE YOUR *.config*. PLEASE MAKE A BACKUP BEFORE DOING THIS"
+
+username=$(whoami)
+while true; do
+        echo "username: $username?"
+        read -p "(y / n) : " answer
+        
+        case "$answer" in
+                [Yy]* )
+                        break
+                        ;;
+                [Nn]* )
+                        read -p "username: " username
+                        ;;
+        
+                * )
+                        ;;
+        esac
+        echo ""
+done
+
+
+if [ ! -d "/home/$username/" ]; then
+        echo "/home/$username/ not exists"
+        exit 1
+fi
+
+
+while true; do
+	read -p "Wanna install the packages (y/n)? " answer
+
+	case "$answer" in
+		[Yy]* )
+			sudo pacman -Sy
+			sudo pacman -Syu
+			sudo pacman -S xdg-desktop-portal xdg-desktop-portal-hyprland wireplumber base base-devel blueman bluez bluez-utils dunst fastfetch firefox flatpak git grim htop hyprland hyprshot kitty nano nemo neovim pavucontrol pipewire pipewire-alsa pipewire-pulse pipewire-jack ranger rofi slurp starship sxiv unzip waybar
+			systemctl --user start pipewire
+			systemctl --user enable pipewire
+			break
+			;;
+		[Nn]* )
+			break
+			;;
+		* )
+			;;
+	esac
+	echo ""
+done
+
+
+while true; do
+	read -p "Wanna use the *.config* from the repo (y/n)? " answer
+
+	case "$answer" in
+		[Yy]* )
+			sudo rm -rf ~/.config
+			sudo cp ./config /home/$username/
+			sudo mv /home/$username/config /home/$username/.config
+			sudo chown $username /home/$username/.config/*
+			break
+			;;
+		[Nn]* )
+			break
+			;;
+		* )
+			;;
+	esac
+	echo ""
+done
+
+
+while true; do
+	read -p "Wanna use the *bash* from the repo (y/n)? " answer
+
+	case "$answer" in
+		[Yy]* )
+			sudo rm -rf /home/$username/.bashrc
+			sudo rm -rf /home/$username/.bash_profile
+			sudo cp ./home/bashrc /home/$username/
+			sudo cp ./home/bashrc /home/$username/
+			sudo mv ./home/bash_profile /home/$username/.bashrc
+			sudo mv ./home/bash_profile /home/$username/.bash_profile
+			sudo chown $username /home/$username/.bashrc
+			sudo chown $username /home/$username/.bash_profile
+			break
+			;;
+		[Nn]* )
+			break
+			;;
+		* )
+			;;
+	esac
+	echo ""
+done
+
+
+while true; do
+	read -p "Wanna wallpaper (y/n)? " answer
+
+	case "$answer" in
+		[Yy]* )
+			read -p "Wanna use *swww* or *hyprpaper* (1/2)" wall
+
+			case "$wall" in
+				[1]* )
+					git clone https://aur.archlinux.org/yay.git /home/$username/
+					cd /home/$username/yay
+					makepkg -si
+					yay -S swww
+					sudo chmod +x /home/$username/.config/hypr/scripts/rofi-wallpaper.sh
+					sudo cp -r ./home/pictures/ /home/$username/
+					break 
+					;;
+				[2]* )
+					sudo pacman -S hyprpaper
+					sudo cp -r ./home/pictures/ /home/$username/
+					break
+					;;
+				* )
+					;;
+			esac
+		[Nn]* )
+			break
+			;;
+		* )
+			;;
+	esac
+	echo ""
+done
+
+
+echo "done!!"
+sudo reboot
